984e7b50e7b58f64570e56838b9b2502c52ccc9d
Ferran <ferran.docencia@gmail.com>
2025-06-18 09:30:08 +0200

    cursos.ejs

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-const express = require('express')
-const app = express()
+const path = require("node:path");
+let ejs = require('ejs');
+const express = require("express");
+const app = express();
 
-process.loadEnvFile()
-const PORT = process.env.PORT || 3000
+process.loadEnvFile();
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('GET request to the homepage')
+// Para usar EJS como motor de plantillas
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
+
+// Indicar la carpeta de los recursos estáticos: css, js, imágenes, ...
+// path.join(__dirname + "/public") <- No aconsejado
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", (req, res) => {
+  //   res.send('<h1>GET request to the homepage</h1>')
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/cursos", (req, res) => {
+    res.render("cursos")
 })
 
 app.listen(PORT, () => {
-    console.log(`Servidor iniciado en http://localhost:${PORT}`);
-})
+  console.log(`Servidor iniciado en http://localhost:${PORT}`);
+});
